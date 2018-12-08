7905a7d4dc9f739543cdd9036c93c607d253e1da
thiamteck <thiamteck@gmail.com>
2018-12-08 12:08:25 +0800

    Fix issues of grub-pc upgrade force interactive mode

--- dev_env_setup/001-os-package-setup/001-package-upgrade.sh
+++ dev_env_setup/001-os-package-setup/001-package-upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 sudo apt-get autoremove -y
